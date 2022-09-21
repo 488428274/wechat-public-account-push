c4fb091fe293cabe470bb2a46fcec60c7d2784f8
488428274 <114044250+488428274@users.noreply.github.com>
2022-09-21 12:21:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 {
 # 公众号配置
 # 公众号appId
-"app_id": " wxd859fcee83b00f95",
+"app_id": "wxd859fcee83b00f95",
 # 公众号appSecret
-"app_secret": " 3cadb459075b190f05327f51c6b9652d",
+"app_secret": "3cadb459075b190f05327f51c6b9652d",
 # 模板消息id
-"template_id": "Te23x1JhHUrhbuR8u6X5Zjxk30TwG0OwegIBmUqxQFk",
+"template_id": "r9fgybl7hKUBpyIo-wu1KwOElv3Xh724pulLD8P1_WM",
 # 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
 "user": ["oiuEE6XQo8dc-mbBnV7G4HS2q9aA"],
 
@@ -15,7 +15,7 @@
 # 所在城市
 "city": "广州",
 # 生日1，修改name为对应需要显示的名字，如果生日为农历，在最前面加上r即可
-"birthday1": {"name": "小宝", "birthday": "r1997-07-20"},
+"birthday1": {"name": "丽丽", "birthday": "r1997-07-20"},
 # 生日2
 "birthday2": {"name": "小林", "birthday": "1997-08-18"},
 # 在一起的日子，格式同上
